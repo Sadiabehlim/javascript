cd3590de1693cee44ad21603eacc5e3b262e5f5e
Sadiabehlim <sadiabehlim.123@gmail.com>
2022-11-01 16:41:26 -0700

    project started

---
+++ app.js
@@ -0,0 +1,49 @@
+alert ( "Welcome to Javascript!" );
+var name = "Sadia"
+console.log(name);
+var nationality;
+nationality = "Pakistani"
+console.log(nationality);
+var lastname = "Rafiq"
+console.log(lastname);
+var nickname = "sadi"
+console.log(nickname);
+var weight = 150;
+var c = weight + 25;
+console.log(c);
+var originalNum = 23;
+var newNum = originalNum + 7;
+console.log(newNum);
+var originalNum = "23";
+var newNum = originalNum + 7;
+console.log(newNum);
+var num = 10;
+var popularNumber = num + 200;
+console.log(popularNumber);
+var whatsLeftover = 9 % 3;
+console.log(whatsLeftover);
+var num = 1;
+var newNum = num++;
+console.log(newNum);
+var num = 1;
+var newNum = ++num;
+console.log(newNum);
+var num = 1;
+var newNum = num--;
+console.log(newNum);
+var num = 1;
+var newNum = --num;
+console.log(newNum);
+var totalcost = 1 + 3 * 4;
+console.log(totalcost);
+var totalcost = 1 +(3 * 4);
+console.log(totalcost);
+var totalcost = (1 + 3) * 4;
+console.log(totalcost);
+var resultofcomputation = (2 * 4) * 4 + 2;
+console.log(resultofcomputation);
+var resultofcomputation = (2 * 4) * (4 + 2);
+console.log(resultofcomputation);
+var message = "I am"
+alert (message + nationality + "!");
+
